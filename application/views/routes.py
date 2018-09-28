1d2c1afe2a2017ae602050e1aea96fc841fda643
userstar713 <userstar713@gmail.com>
2018-09-28 13:16:56 +0300

    added scraper

--- application/views/routes.py
+++ application/views/routes.py
@@ -4,6 +4,9 @@ from .helpers import seller_integration_bp
 
 from ..tasks import start_synchronization
 
+from application.tasks.spiders import execute_spider
+
+
 @seller_integration_bp.route(
     '/status'
 )
@@ -15,16 +18,29 @@ def index():
 
 @seller_integration_bp.route(
     '/source/<int:source_id>/sync/',
-    methods=['POST']
 )
 def sync(source_id: int) -> Response:
     content = request.get_json()
-    callback_url = content['callback_url']
+    # callback_url = content['callback_url']
     status = start_synchronization(source_id)
     return jsonify(
         {'data': {
             'status': status,
-            'callback_url': callback_url
+            # 'callback_url': callback_url
+        }
+        }
+    )
+
+
+@seller_integration_bp.route(
+    '/source/<int:source_id>/scrape/',
+)
+def scrape(source_id: int) -> Response:
+    status = execute_spider(source_id)
+    return jsonify(
+        {
+            'data': {
+                'status': status,
             }
         }
     )
