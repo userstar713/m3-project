35bca6118e86f65f613ca3adad9f0edf6a0ea6fa
userstar713 <userstar713@gmail.com>
2018-10-30 16:40:13 +0200

    M3-289: update product summarization; added spacy package and it's dependencies

--- application/summarization.py
+++ application/summarization.py
@@ -1,7 +1,16 @@
 import json
+import re
+import spacy
 
+
+CONTAIN_VALUE_REG = re.compile(r'.*[0-9][0-9].*')
+CONTAIN_FUZZY_BRACKETS_REG = re.compile(r'.*\(.*\).*')
+CONTAIN_POINTS_REG = re.compile(r'.*points.*')
+CONTAIN_VALUE_BRACKETS_PLUS_REG = re.compile(r'[0-9][0-9]\(.*\)')
+CONTAIN_VALUE_PLUS_REG = re.compile(r'.*[4-9][0-9][+].*')
+CONTAIN_STRICT_BRACKETS_REG = re.compile(r'\(.*\)')
+NLP = spacy.load('en_core_web_sm')
 WEIGHT_DIC = dict()
-
 ATTRIBUTES = ("sku",
               "designation",
               "price",
@@ -47,8 +56,42 @@ for weight in ATTRIBUTES:
     WEIGHT_DIC[weight.strip()] = 1.0
 
 
+def is_valid_summary(summary):
+    rule_1 = CONTAIN_VALUE_REG.match(summary)
+    rule_2 = CONTAIN_FUZZY_BRACKETS_REG.match(summary)
+    rule_3 = CONTAIN_POINTS_REG.match(summary)
+    rule_4 = CONTAIN_VALUE_BRACKETS_PLUS_REG.match(summary)
+    if rule_1 is not None:
+        return False
+    if rule_2 is not None:
+        return False
+    if rule_3 is not None:
+        return False
+    if rule_4 is not None:
+        return False
+    return True
+
+
+def is_valid_sentence(sentence):
+    rule_3 = CONTAIN_POINTS_REG.match(sentence)
+    rule_4 = CONTAIN_VALUE_BRACKETS_PLUS_REG.match(sentence)
+    rule_5 = CONTAIN_VALUE_PLUS_REG.match(sentence)
+    rule_6 = CONTAIN_STRICT_BRACKETS_REG.match(sentence)
+
+    if rule_3 is not None:
+        return False
+    if rule_4 is not None:
+        return False
+    if rule_5 is not None:
+        return False
+    if rule_6 is not None:
+        if rule_6.group() == sentence:
+            return False
+    return True
+
+
 def scoring(structured_data):
-    summery_list = []
+    summary_list = []
     for data in structured_data:
         summery_dic = []
         for i, sentence in enumerate(data['sentences']):
@@ -69,9 +112,9 @@ def scoring(structured_data):
             sentence_dic['text'] = text
             sentence_dic['score'] = score
             summery_dic.append(sentence_dic)
-        summery_list.append(
+        summary_list.append(
             sorted(summery_dic, key=lambda x: x['score'], reverse=True))
-    return summery_list
+    return summary_list
 
 
 def adopt(sentences_dic, max_words_len):
@@ -88,6 +131,19 @@ def adopt(sentences_dic, max_words_len):
     return summery_dic
 
 
+def remove_unnecessary_information(summary):
+    final_result = ''
+    if not is_valid_summary(summary):
+        doc = NLP(summary)
+        for sent in doc.sents:
+            if is_valid_sentence(sent.text):
+                final_result += sent.text.strip() + ' '
+    else:
+        final_result = summary
+
+    return final_result.strip()
+
+
 def extract_text(sorted_sum_dic):
     summery_text = ''
     score = 0.0
@@ -98,27 +154,36 @@ def extract_text(sorted_sum_dic):
 
 
 def summarize(structured_data, max_words_len=30):
-    summery_list = []
+    summary_list = []
     sum_dic_list = scoring(structured_data)
     for sum_dic in sum_dic_list:
         text, score = extract_text(adopt(sum_dic, max_words_len))
         summery_dic = dict()
         summery_dic['text'] = text
         summery_dic['score'] = score
-        summery_list.append(summery_dic)
-    summery_list = sorted(summery_list, key=lambda x: x['score'],
+        summary_list.append(summery_dic)
+    summary_list = sorted(summary_list, key=lambda x: x['score'],
                           reverse=True)  # get the highest score summery
-    best_summery = summery_list[0]
-    summery_text = best_summery['text']
-    summery_score = best_summery['score']
-    if best_summery['score'] == 0.0 and best_summery['text']!='' and max_words_len!=50:
-        summery_text,summery_score = summarize(structured_data, 50)
-    return summery_text, summery_score
+    best_summery = summary_list[0]
+    summary_text = best_summery['text']
+    summary_score = best_summery['score']
+    if (best_summery['score'] == 0.0 and
+            best_summery['text'] != '' and
+            max_words_len != 50):
+        summary_text, summary_score = summarize(structured_data, 50)
+    summary_text = remove_unnecessary_information(summary_text)
 
-if __name__ == "__main__":
+    return summary_text, summary_score
+
+
+def main():
     with open('output.json', 'r') as f:
         json_data = json.load(f)
         for product in json_data.values():
-            summery,score=summarize(product)
-            if score==0.0 and summery!='':
-                print(json.dumps(summarize(product, 50), indent=4), end="\n\n")
+            summary, _ = summarize(product)
+            if summary != '':
+                print(summary)
+
+
+if __name__ == "__main__":
+    main()
