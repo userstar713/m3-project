6f731f1d3952f08b268de752198ae91f936d4125
userstar713 <userstar713@gmail.com>
2018-12-04 18:44:12 +0200

    Increase celery worker concurrency to 10

--- application/tasks/__init__.py
+++ application/tasks/__init__.py
@@ -6,7 +6,7 @@ def init_celery(app):
     # set broker url and result backend from app config
     celery.conf.broker_url = app.config['CELERY_BROKER_URL']
     celery.conf.result_backend = app.config['CELERY_RESULT_BACKEND']
-    celery.conf.worker_concurrency = 3
+    celery.conf.worker_concurrency = 10
     if app.config['CELERY_EAGER']:
         celery.conf.task_always_eager = True
         celery.conf.task_eager_propagates = True
