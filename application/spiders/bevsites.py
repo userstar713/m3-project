295ccefd515683e5eb21a0dfe38e40552206868c
userstar713 <userstar713@gmail.com>
2019-05-02 19:30:22 -0700

    Update bevsites.py

--- application/spiders/bevsites.py
+++ application/spiders/bevsites.py
@@ -4,5 +4,5 @@ from application.scrapers.csvurl_scraper import CSVURLScraper
 BEVSITES_DATA_URL = 'https://dl.dropbox.com/s/keowy5llbuea3yk/added_review_bevsites.csv'
 # Pass in the data_url to csv to parent CSVURLScraper class and that's it
 class BevsitesCSVScraper(CSVURLScraper):
-    def __init__(self,):
-        super().__init__(data_url=BEVSITES_DATA_URL)
+    def __init__(self):
+        super().__init__(BEVSITES_DATA_URL)
